3c54c6fcaad7eab6b330316e4a919bf26aac737a
rabiulislam <2012rabiul@gmail.com>
2023-03-26 13:09:45 +0600

    Documentation info updated.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,7 +8,22 @@ use Illuminate\Routing\Controller as BaseController;
 use OpenApi\Annotations as OA;
 
 /**
- * @OA\Info(title="Authentication API", version="1.0")
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Pet Shop",
+ *     description="Pet shop API documentation",
+ *     @OA\Contact(
+ *         name="Rabiul Islam",
+ *         email="rir.cse.71@gmail.com"
+ *     ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * ),
+ * @OA\Server(
+ *     url="/api/v1",
+ * ),
  */
 class Controller extends BaseController
 {
